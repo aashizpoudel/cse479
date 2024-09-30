2ba72b033c87d03461fd10ad5ebe8de20040c2d1
Aashish <mr.aashiz@gmail.com>
2024-09-29 22:46:53 -0500

    Added space for readability. Removed break statement.

--- hw1/main.py
+++ hw1/main.py
@@ -17,18 +17,23 @@ for batch_arg,batch_size_val in batch_sizes.items():
         for regularizer_arg,use_regularizer in regularizers.items():
             print(f"Working on model arch 1,[{batch_arg},{optim_arg},{regularizer_arg}]")
             prefix = f"{batch_arg}_{optim_arg}_{regularizer_arg}"
+            
             exp_model = model.model_1(use_regularizer=use_regularizer)                              #load model architecture
             exp_model.compile(optimizer=optimizer_var,loss=loss_fn,metrics=['accuracy'])
+            
             train_ds = utils.get_training_set(batch_size=batch_size_val)                            # get train dataset
             val_ds = utils.get_val_set(batch_size=batch_size_val)                                   # get val dataset
             csv_logger_cb = tf.keras.callbacks.CSVLogger(filename=f"logs/{prefix}_experiment_log.csv")   # store logs in csv file
             exp_model.fit(train_ds,epochs=3,validation_data=val_ds,callbacks=[early_stopping_cb,csv_logger_cb]) # model training
+            
             exp_model.save_weights(f"logs/{prefix}_best_weight.weights.h5") #saving the best model
+            
             evaluation_results = utils.evaluate(exp_model)   #perform evaluation on test dataset 
+            
             pickle.dump(evaluation_results,open(f"logs/{prefix}_evaluation_results.dat","wb"))  #save confusion matrix in file for future plotting
             print(f"Model arch 1,[{batch_arg},{optim_arg},{regularizer_arg}]=>Accuracy:{evaluation_results[0]:.3f},F1:{evaluation_results[1]:.3f}") # printing
             del exp_model #deleting model variable to recreate new model
-            break
+            
             
 
 
