c844bb7b7ee551c921e882ff49ccf7d1a514a71e
Aashish <mr.aashiz@gmail.com>
2024-09-29 22:42:09 -0500

    added model loading codes

--- hw1/model.py
+++ hw1/model.py
@@ -1,10 +1,35 @@
 import tensorflow as tf 
 
 
-def model_1():
+#Design model architecture here.
+def model_1(use_regularizer=False):
+    # Architecture 1
+    if use_regularizer:
+        model = tf.keras.Sequential([
+            tf.keras.layers.Flatten(input_shape=(28,28)),
+            tf.keras.layers.Dense(100, activation="relu",kernel_regularizer=tf.keras.regularizers.L1()),
+            tf.keras.layers.Dense(100, activation="relu",kernel_regularizer=tf.keras.regularizers.L1()),
+            tf.keras.layers.Dense(10)
+        ])
+    else:
+        model = tf.keras.Sequential([
+            tf.keras.layers.Flatten(input_shape=(28,28)),
+            tf.keras.layers.Dense(100, activation="relu"),
+            tf.keras.layers.Dense(100, activation="relu"),
+            tf.keras.layers.Dense(10)
+        ])
+    return model
+
+
+#another model architecture
+def model_2():
     pass 
 
 
+#another model architecture
+def model_3():
+    pass 
 
-def model_2():
+
+def model_4():
     pass 
