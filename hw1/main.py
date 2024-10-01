87eb5df25ad752ec04453ef0ed1f824c79f4c809
Aashish <mr.aashiz@gmail.com>
2024-10-01 11:43:29 -0500

    done minor edits

--- hw1/main.py
+++ hw1/main.py
@@ -5,12 +5,17 @@ import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Check if there is folder of logs because everything will be stored in logs folder.
+utils.dir_check("./logs")
 
+#Set random seed of 1024 for reproducibility
 tf.random.set_seed(1024)
+
 loss_fn = tf.keras.losses.CategoricalCrossentropy()
 early_stopping_cb = tf.keras.callbacks.EarlyStopping(patience=4, restore_best_weights=True)
 
-# Experiment with first architecture
+
+
 models = {1 : model.model_1, 2 : model.model_2}
 learning_rates = {'0.01':0.01,"0.001":0.001}
 batch_sizes = {'batch_8':8, 'batch_32':32}
@@ -22,40 +27,43 @@ for model_num,model_fn in models.items():
             for regularizer_arg,use_regularizer in regularizers.items():
                 single_result = {'model':model_num,'learning_rate':lr_arg,'batch_size':batch_size_val,'regularizer':use_regularizer}
                 
-                print(f"Working on model arch 1,{batch_arg},{lr_arg},{regularizer_arg}]")
-                prefix = f"1_{batch_arg}_{lr_arg}_{regularizer_arg}"
+                print(f"Working on model arch {model_num},{batch_arg},{lr_arg},{regularizer_arg}]")
+                prefix = f"{model_num}_{batch_arg}_{lr_arg}_{regularizer_arg}"
                 
                 exp_model = model_fn(use_regularizer=use_regularizer)                              #load model architecture
                 exp_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr_val),loss=loss_fn,metrics=['accuracy'])
                 
                 train_ds = utils.get_training_set(batch_size=batch_size_val)                            # get train dataset
-                val_ds = utils.get_val_set(batch_size=batch_size_val)                                   # get val dataset
-                if utils.dir_check(f"logs/{prefix}_experiment_log.csv"): 
-                    csv_logger_cb = tf.keras.callbacks.CSVLogger(filename=f"logs/{prefix}_experiment_log.csv")   # store logs in csv file
+                val_ds = utils.get_val_set(batch_size=batch_size_val)  
+                # get val dataset
+                
+                csv_logger_cb = tf.keras.callbacks.CSVLogger(filename=f"logs/{prefix}_experiment_log.csv")   # store logs in csv file
+                
                 exp_model.fit(train_ds,epochs=200,validation_data=val_ds,callbacks=[early_stopping_cb,csv_logger_cb])
-                if utils.dir_check(f"logs/{prefix}_best_weight.weights.h5"):
-                    exp_model.save_weights(f"logs/{prefix}_best_weight.weights.h5") #saving the best model
+                
+                exp_model.save_weights(f"logs/{prefix}_best_weight.weights.h5") #saving the best model for this run
                 
                 evaluation_results = utils.evaluate(exp_model,val_ds)   #perform evaluation on val dataset 
                 
-                print(f"Model arch 1,[{batch_arg},{lr_arg},{regularizer_arg}]=>Accuracy:{evaluation_results[0]:.3f},F1:{evaluation_results[1]:.3f}") # printing
+                print(f"Model arch {model_num},[{batch_arg},{lr_arg},{regularizer_arg}]=>Accuracy:{evaluation_results[0]:.3f},F1:{evaluation_results[1]:.3f}") # printing
+                
                 single_result['val_accuracy'] = evaluation_results[0]
                 single_result['val_f1_score'] = evaluation_results[1]
-                if utils.dir_check(f"logs/{prefix}_experiment_log.csv"):
-                    single_result['converged_on']= pd.read_csv(f"logs/{prefix}_experiment_log.csv").tail(1)['epoch'].item()
+                single_result['converged_on']= pd.read_csv(f"logs/{prefix}_experiment_log.csv").tail(1)['epoch'].item()
+                single_result['prefix'] = prefix
                 results_csv.append(single_result)
-                if utils.dir_check(f"logs/{prefix}_experiment_log.csv"):
-                    train_history = pd.read_csv(f"logs/{prefix}_experiment_log.csv")
+                
+                train_history = pd.read_csv(f"logs/{prefix}_experiment_log.csv")
                 fig,axs = plt.subplots()
                 axs.plot(train_history['loss'],label="Training loss")
                 axs.plot(train_history['val_loss'],label="Validation loss")
-                plt.title(f"Training history for batch size={batch_size_val}, learning_rate={lr_val}, regularizer={use_regularizer}.\n val accuracy {evaluation_results[0]:.3f})")
+                plt.title(f"Training history - Model:{model_num}\n Hyps: batch size={batch_size_val}, learning_rate={lr_val}, regularizer={use_regularizer}\n Val accuracy {evaluation_results[0]:.3f})")
                 plt.xlabel("Epochs")
                 plt.ylabel("Loss")
                 plt.legend()
                 plt.savefig(f"logs/{prefix}_training_history.pdf")
                 # plt.show()
-                
+                plt.close()
                 
                 del exp_model #deleting model variable to recreate new model
             
@@ -67,15 +75,6 @@ final_result.to_csv("logs/exp1.csv")
 
 
 
-
-
-
-# Experiment with second architecture
-
-
-
-
-# Experiment with third architecture
 
 
 
